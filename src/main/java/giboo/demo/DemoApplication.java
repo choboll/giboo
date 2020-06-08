7824fe4c645bba8db25f82606d65ff41175c73e0
choboll <47838478+choboll@users.noreply.github.com>
2020-06-08 14:38:00 +0800

    Update DemoApplication.java

--- src/main/java/giboo/demo/DemoApplication.java
+++ src/main/java/giboo/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "您好！";
     }
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
